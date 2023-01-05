28ed65cfc2cef65ee7d9ce84bd5fd37cbeb4f070
Connor McCarthy <connor15mcc@gmail.com>
2023-01-05 15:40:05 -0500

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga4';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+
+ReactGA.initialize('G-SGE8J7VF5N', {
+  gaOptions: {
+    sampleRate: 100,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
